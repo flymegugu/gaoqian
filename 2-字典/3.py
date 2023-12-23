9e5ed0cb5322a6c430dcda691cede75c95253443
MaxGu <flyme007@yeah.net>
2023-12-23 22:34:13 +0800

    add

---
+++ 2-字典/3.py
@@ -0,0 +1,12 @@
+'''
+Date: 2023-12-23 22:24:09
+LastEditors: MaxGu flyme007@yeah.net
+LastEditTime: 2023-12-23 22:32:58
+FilePath: /tools/2-字典/3.py
+Description: 
+'''
+tinydict = {"name":"test","age":12,"class":"first"}
+tinydict['age'] = 0
+tinydict['school'] = "菜鸟教程"
+print(tinydict['age'])
+print(tinydict['school'])
---
+++ 2-字典/4.py
@@ -0,0 +1,9 @@
+'''
+Date: 2023-12-23 22:30:10
+LastEditors: MaxGu flyme007@yeah.net
+LastEditTime: 2023-12-23 22:30:14
+FilePath: /tools/2-字典/4.py
+Description: 
+'''
+
+test
