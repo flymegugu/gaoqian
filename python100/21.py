d13e619fffffc40e06e31b16e1b60fc27d6d2ce2
MaxGu <flyme007@yeah.net>
2024-01-20 18:23:05 +0800

    add

---
+++ python100/21.py
@@ -0,0 +1,8 @@
+'''
+Date: 2024-01-20 18:19:46
+LastEditors: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
+LastEditTime: 2024-01-20 18:19:49
+FilePath: /tools/python100/21.py
+Description: 
+'''
+
